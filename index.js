118829f71f3bbf5de6e1aca908a2b45dcbd81335
Brian Nicholas <uibri@me.com>
2018-04-26 20:17:48 -0700

    booya

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (require('dotenv').config({ silent: process.env.NODE_ENV === 'production' }))
 const fs = require('fs')
-
+const path = require('path')
 const fileData = JSON.stringify({
   private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_CLIENT_EMAIL
@@ -11,139 +11,92 @@ const express = require('express')
 const app = express()
 
 const imgPath = 'images/DSC_5454.jpg'
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.Promise = global.Promise;
-const gridfs = require('gridfs-stream');
-gridfs.mongo = mongoose.mongo;
 const connection = mongoose.connection
-const multer = require('multer')
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const ObjectId = mongoose.Types.ObjectId
 
 const vision = require('@google-cloud/vision')
 const client = new vision.ImageAnnotatorClient()
 
-app.get('/vision-test', (req, res) => {
-  client
-  .labelDetection('./images/test.jpg')
-  .then(results => {
-    console.log(results[0].labelAnnotations)
-    res.send(results[0].labelAnnotations)
-  })
-  .catch(err => {
-    console.log(err);
-    res.send({Error: err})
-  })
-})
+const clear = require("cli-clear")
 
+function getVisionData(img) {
+  return new Promise(function(resolve, reject) {
+    client
+    .labelDetection(img)
+    .then(results => resolve(results[0].labelAnnotations))
+    .catch(err => reject(err))
+  })
+}
 
 connection.once('open', function () {
   console.log('mongodb connection OPEN');
+  const multer = require('multer')
+  const Grid = require('gridfs-stream');
+  Grid.mongo = mongoose.mongo
+  const gfs = Grid(connection.db)
+  const GridFsStorage = require('multer-gridfs-storage')
+  gfs.collection('photos')
 
+  const storage = new GridFsStorage({
+    url: process.env.MONGODB_URI,
+    file: (req, file) => {
+      if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        return {
+          bucketName: 'photos',
+          filename: Date.now() + path.extname(file.originalname)
+        };
+      } else {
+        return null;
+      }
+    }
+  })
+  const upload = multer({ storage });
 
-  const gfs = gridfs(connection.db)
-
-  const Attachment = mongoose.model('Photos', {
+  const PhotoModel = mongoose.model('Photos', {
+    fileID: String,
     fileName: String,
-    labels:   Array,
-    image: { data: Buffer, contentType: String }
+    fileURL: String,
+    fileContentType: String,
+    labels: Array
   })
-
-  const writestream = gfs.createWriteStream({
-    filename: 'mongo_file.txt'
-  })
-
 
   app.get('/', (req, res) => res.send('hello world'))
 
-  app.get('/clear', (req, res) => {
-    ImageModel.remove(function (err) {
-      if (err) res.send(err);
-      else {
-        ImageModel.find(function (err, records) {
-          if (err) res.send(err);
-          else res.send(records)
+  app.post('/upload', upload.single('photo'), function (req, res) {
+
+    gfs.files.findOne({ _id: req.file.id }, (err, file) => {
+      console.log(file);
+      const readstream = gfs.createReadStream(file.filename)
+      let buffer = new Buffer(0)
+      readstream.on('data', chunk => buffer = Buffer.concat([buffer, chunk]))
+      readstream.on('end', () => {
+        getVisionData(buffer)
+        .then(data => {
+          let photo = new PhotoModel
+          photo.fileID = req.file.id
+          photo.fileURL = `/image/${req.file.filename}`
+          photo.fileName = req.file.filename
+          photo.fileContentType = req.file.mimetype
+          photo.labels = data.map(item => item.description)
+          photo.save((err, photo) => {
+            console.log('photo.save');
+            console.log(photo);
+            res.send(photo)
+          })
         })
-      }
-    })
-  })
-
-  app.get('/test', (req, res) => {
-    const testImg = new ImageModel
-    testImg.img.data = fs.readFileSync(imgPath)
-    testImg.img.contentType = 'image/jpg'
-    testImg.save((err, testImg) => {
-      if (err) throw err
-      else {
-        res.contentType(testImg.img.contentType)
-        res.send(testImg.img.data)
-      }
-    })
-  })
-
-  app.post('/write', upload.single('photo'), function (req, res) {
-
-    // console.log('----------------------------');
-    console.log(req.file)
-    // // res.send('ok')
-
-    const writestream = gfs.createWriteStream({
-      filename: req.file.originalname,
-      mode: 'w',
-      content_type: req.file.mimetype,
-      metadata: req.body
-    })
-
-    req.pipe(writestream)
-
-    writestream.on('close', file => {
-      console.log('\n ========================= \n');
-      console.log(file)
-      res.send('writestream.on close')
-
-      // file._id ... reference the fs.files id in the photos document
-      // const testImg = new Attachment
-      // testImg.fileName = file.filename
-      // testImg.image = file
-      // testImg.save((err, testImg) => {
-      //   if (err) res.send(err)
-      //   else {
-      //     res.contentType(testImg.img.contentType)
-      //     res.send(testImg)
-      //   }
-      // })
-
-      // fs.unlink(req.file.path, function(err) {
-      //   // handle error
-      //   console.log('success!')
-      // })
-    })
-
-  })
-
-  app.post('/upload', (req, res) => {
-    let writestream = gfs.createWriteStream({ filename: db_filename });
-    fs.createReadStream(local_file).pipe(writestream);
-    writestream.on('close', function (file) {
-      res.send('File Created : ' + file.filename);
-    })
-
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString() // convert Buffer to string
-    })
-    req.on('end', () => {
-      const testImg = new ImageModel
-      testImg.img.data = body
-      testImg.img.contentType = 'image/jpg'
-      testImg.save((err, testImg) => {
-        if (err) res.send(err)
-        else {
-          res.contentType(testImg.img.contentType)
-          res.send(testImg.img.data)
-        }
+        .catch(err => res.send(err))
       })
+    })
+  })
+
+  app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      const readstream = gfs.createReadStream(file.filename)
+      readstream.pipe(res)
     })
   })
 
