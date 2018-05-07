0b12dfbaf6a0e056b9bec07ce6d3709ac578ab88
Brian Nicholas <uibri@me.com>
2018-05-06 17:56:12 -0700

    added delete functionality, updated UI

--- public/vision.js
+++ public/vision.js
@@ -9,16 +9,9 @@ const formElement = document.getElementById('uploader')
 const searchElement = document.getElementById('search')
 const imageDataElement = document.getElementById('image_data')
 
-const imageStoreEvents = {
-  set: (obj, prop, value) => {
-    if (prop === 'visible') {
-      if (value === false) obj.el.classList.add('hide')
-      if (value === true) obj.el.classList.remove('hide')
-    }
-  }
-}
-
 function fetchAllPhotos() {
+  markup = []
+  imageStore = {}
   fetch(allImagesURL, { method: 'GET' })
   .then(response => response.json())
   .then(json => {
@@ -27,74 +20,87 @@ function fetchAllPhotos() {
   })
 }
 
+function buildImageStore(records) {
+  records.forEach(record => addImageToStore(record))
+}
+
 function loadAllPhotos(photos) {
   photos.forEach(record => addImageElementToList(record))
   updateHtml()
+  addImageHandlers()
 }
 
-// function addImageElementToList(record) {
-//   let section = []
-//   section.push(`<section id="${record._id}">`)
-//   section.push(`<img class="thumbnail" src="${record.fileURL}" />`)
-//   section.push('<ul>')
-//   record.labels.forEach(item => section.push(`<li>${item.label} : ${item.score}</li>`))
-//   section.push('</section>')
-//   markup = section.concat(markup)
-//   return section
-// }
+function updateHtml() {
+  imageDataElement.innerHTML = markup.join('\n')
+}
 
 function addImageElementToList(record) {
   let section = []
   let classNames = ['thumbnail']
   if (record.exif && record.exif.gps) classNames.push('gps')
   section.push(`<section id="${record._id}" class="image">`)
+  section.push(`<a href="" data-record="${record._id}" class="delete">x</a>`)
   section.push(`<img class="${classNames.join(' ')}" src="${record.fileURL}" />`)
   section.push(`<p style="display: none">${JSON.stringify(record)}</p>`)
   section.push('</section>')
   markup = section.concat(markup)
-  return section
 }
 
-// THIS IS BLOWING AWAY MY el references in the storage object
-function updateHtml() {
-  imageDataElement.innerHTML = markup.join('\n')
+function deleteImage(id) {
+  let url = `http://${window.location.host}/api/image/${id}`
+  fetch(url, {method: 'DELETE'})
+  .then(response => response.text())
+  .then(text => fetchAllPhotos())
+  .catch(error => console.log(error))
 }
 
-function appendHtml(html) {
-  if (typeOf.Array(html)) {
-    html = html.join('\n')
-  }
+function addImageHandlers() {
+  let deleteLinks = document.querySelectorAll('a.delete')
+  deleteLinks.forEach(el => {
+    el.addEventListener('click', evt => {
+      evt.preventDefault()
+      let id = evt.target.dataset.record
+      deleteImage(id)
+    })
+  })
 }
 
 function hasExif(record) {
   if (record.exif.gps) console.log('EXIF', record.exif.gps)
 }
 
+// const imageStoreEvents = {
+//   set: (obj, prop, value) => {
+//     if (prop === 'visible') {
+//       if (value === false) obj.el.classList.add('hide')
+//       if (value === true) obj.el.classList.remove('hide')
+//     }
+//   }
+// }
+
 function addImageToStore(record) {
   if (record.exif) hasExif(record)
-
   let justLabels = record.labels.map(item => item.label)
   imageStore[record._id] = new Proxy({
     id: record._id,
     labels: justLabels.join(' '),
     visible: true,
     el: document.getElementById(record._id)
-  }, imageStoreEvents)
-}
-
-function buildImageStore(records) {
-  records.forEach(record => addImageToStore(record))
+  }, {
+    set: (obj, prop, value) => {
+      if (prop === 'visible') {
+        if (value === false) obj.el.classList.add('hide')
+        if (value === true) obj.el.classList.remove('hide')
+      }
+    }
+  })
 }
 
 function fetchVisionData() {
   const form = new FormData(document.getElementById('uploader'));
   fetch(uploadURL, { method: 'POST', body: form })
   .then(response => response.json())
-  .then(json => {
-    addImageElementToList(json)
-    updateHtml()
-    addImageToStore(json)
-  })
+  .then(json => fetchAllPhotos())
   .then(() => formElement.reset())
   .catch(error => console.log('ERROR', error))
 }
@@ -105,7 +111,6 @@ fileField.addEventListener('change', evt => {
 
 searchElement.addEventListener('input', evt => {
   let searchFor = evt.target.value.toLowerCase()
-  console.log(searchFor)
   for (let [key, value] of Object.entries(imageStore)) {
     if (value.labels.includes(searchFor)) {
       imageStore[key].visible = true
