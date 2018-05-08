bf53e687f569165a09fb9813afbd4e66da3648dc
Brian Nicholas <uibri@me.com>
2018-05-07 23:15:19 -0700

    try again

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ async function processUpload(gfsParams) {
     thumbnail: thumbnail
   }
   let photo = await createPhoto(props, gfsPhoto).catch(err => console.log(err))
-  console.log(photo.thumbnail)
+  // console.log(photo.thumbnail)
   console.log(photo.thumbnailURL)
   return photo
 }
@@ -202,7 +202,7 @@ function gmToBuffer (data) {
       stdout.on('data', (chunk) => { chunks.push(chunk) })
       // these are 'once' because they can and do fire multiple times for multiple errors,
       // but this is a promise so you'll have to deal with them one at a time
-      stdout.once('end', () => { resolve(Buffer.concat(chunks)) })
+      stdout.on('end', () => { resolve(Buffer.concat(chunks)) })
       // stderr.once('data', (data) => { reject(String(data)) })
     })
   })
